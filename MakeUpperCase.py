67c33bcceef5679381d0262de57c46df5c6f1f97
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-04-21 23:48:37 -0400

    Update MakeUpperCase.py
    Number string contingencies.

--- MakeUpperCase.py
+++ MakeUpperCase.py
@@ -4,9 +4,9 @@ def make_upper_case(s: str):
 # Alternatively...
 
 def make_upper_case(s: str):
-    if type(s) != str:
+    if type(s) != str: # Will try to use another built in method for number strings.
         return print("Incorrect data type. Please try again.")
-
+        
     return print(s.upper())  # Make uppercase
 
 make_upper_case('stop')
