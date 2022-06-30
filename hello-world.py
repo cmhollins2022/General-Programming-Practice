17865a3786abfbb5db0313e425c96d706df22503
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-06-29 21:04:32 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -7,7 +7,7 @@
 # Ok, rise up.
 # What's up, programmer. Hello world.
 # Hey, Ho... Let's go! Generally speaking.
-# Lets go!
+# Lets go! How do you eat an elephant?
 # def greet(name: str):
 #     # Ask the user for their name...
 #     name_answer: str = input(f"Hello, World!, What is your name, user {name}? ")
