fd6f9ee7185d6a2bbd5c9087e7c8f79f4a2ba823
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-07-18 20:18:26 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,7 @@
 # Objective: Define a variable and create a prompt that asks for a users name, then says hello "Name".
 # Goals: If possible, continue to create programs primarily with Pseudo-Code first, and then enhance them by creating
 # a functioning testing environment. Perhaps begin including Object Oriented Programming as well?
-# Keep going
+# Keep going... keep
 # To begin, create a function that takes in two arguments, "self, name"
 # def greet(name: str):
 #     # Ask the user for their name...
