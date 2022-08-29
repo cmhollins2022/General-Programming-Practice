05f3101de3fb89ba14afd7649a1d4ba93fb3bc11
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-08-28 23:23:07 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -2,6 +2,7 @@
 # Goals: If possible, continue to create programs primarily with Pseudo-Code first, and then enhance them by creating
 # a functioning testing environment. Perhaps begin including Object Oriented Programming as well? And also...
 # To begin, begin. Then create a function that takes in two arguments, "self, name"
+# Continue to step it up, then iterate through the string.
 # def greet(name: str):
 #     # Ask the user for their name...
 #     name_answer: str = input(f"Hello, World!, What is your name, user {name}? ")
