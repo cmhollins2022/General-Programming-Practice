c929b41f879f6efa52b7be0b3ed8ea3cc0193e90
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-09-10 23:00:11 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -12,7 +12,7 @@
 #     greet('PyCharm')
 
 # Let's now try some object oriented programming again... in an attempt to get back into the
-# basic concepts, and into the swing of things.
+# basic concepts, and getting into the swing of things.
 
 class User:
     """Properties"""
