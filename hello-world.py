59db718ff79fb802de8fc467637e25ac6ff00114
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-09-25 22:47:41 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -3,7 +3,7 @@
 # a functioning testing environment. Perhaps begin including Object Oriented Programming as well? And also...
 # To begin, begin. Then create a function that takes in two arguments, "self, name"
 # def greet(name: str):
-#     # Ask the user for their name...
+#     # Ask the user for their name... documentation
 #     name_answer: str = input(f"Hello, World!, What is your name, user {name}? ")
 #     print(f"Hello {name_answer}, it's so nice to meet you!")  # Print out a greeting message
 #
